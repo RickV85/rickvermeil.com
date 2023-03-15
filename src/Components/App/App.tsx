1a55c01d3998ac77855900c7c8726afba02047d9
Rick Vermeil <rickv85@gmail.com>
2023-03-15 16:35:39 -0400

    Add background to home to App and manifest JSON

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -5,7 +5,7 @@ import Home from '../Home/Home';
 
 function App() {
   return (
-    <main className='font-body'>
+    <main className='font-body bg-beige h-screen'>
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
