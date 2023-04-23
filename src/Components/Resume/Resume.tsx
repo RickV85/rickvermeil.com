95eaaa09da5e1b6ced68236a802f93e3572d4277
Rick Vermeil <rickv85@gmail.com>
2023-04-23 12:55:09 -0600

    Switch resume to googledoc iframe

--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -18,7 +18,7 @@ export default function Resume({section, setSection}: Props) {
 
   return (
     <section>
-      <iframe className="h-[81vh] md:h-[100vh] w-screen" title="resume" src={'https://cvws.icloud-content.com/B/Af6DpWjoTDjywO_ABWJLJnxl8s1yARzUYPx-yqyd0ivEbZoLIB2FiEw2/Rick+Vermeil+Resume.pdf?o=ApA9O28eRE_nsUSVp7u_p1Xc7p1PF0_M9n8oviikqPqh&v=1&x=3&a=CAogI2RLnAXhyCaj5CFGvZArfVMen4KdygjSWSMRR41ROZASaRD_zuD4-jAY_6u8-vowIgEAUgRl8s1yWgSFiEw2aiQMA-kJ3BkaSHyivESzaoZuAAMYdUiiMY-mr6kokiQxrdYFEs5yJLW0CA51hUcB-guRgovnEZrZe7yOvTmZEJQdasyQAydGUA6PQQ&e=1682273408&fl=&r=bf836738-5ac0-471f-955c-797b571c0fe2-1&k=oGPb2KhQxTYIibDUneyXpg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=72&s=B0fF48rkyEJQx6It0OKk4XI5euo&cd=i'} />
+      <iframe className="h-[81vh] md:h-[100vh] w-screen" title="resume" src={'https://docs.google.com/document/d/1JpoXZB_NuWK4_PNt6BpWsSTN8Qa3XJu9tk0dLGC6bDk/edit?usp=sharing'} />
     </section>
   )
 
