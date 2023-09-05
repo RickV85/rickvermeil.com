d2f902f1e4f7c38d5545d0da4b7be42f05267481
Rick Vermeil <rickv85@gmail.com>
2023-09-05 09:53:55 -0400

    Add fade in/up effect to home bio copy

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import profile from "../../assets/profile pic.webp";
 import HeroImage from "../HeroImage/HeroImage";
 import { useNavigate } from "react-router-dom";
@@ -6,8 +6,11 @@ import { useNavigate } from "react-router-dom";
 function Home() {
   const heroImageUrls = ["/assets/Shelf-81.webp","/assets/DSC_0232.webp", "/assets/Cropped_Keystone_Enduro_EvenFlow.webp"];
   const [heroImgIndex, setHeroImgIndex] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+  const paragraphRef = useRef(null);
 
   const navigate = useNavigate();
+
 
   useEffect(() => {
     const heroImgTimer = setInterval(() => {
@@ -17,6 +20,25 @@ function Home() {
       clearInterval(heroImgTimer);
     };
   }, [heroImgIndex, heroImageUrls.length]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry], obs) => {
+        if (entry.target === paragraphRef.current && entry.isIntersecting) {
+          setIsVisible(true);
+          obs.unobserve(paragraphRef.current);
+        }
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.15
+      }
+    );
+    if (paragraphRef.current) {
+      observer.observe(paragraphRef.current);
+    }
+  }, []);
 
   return (
     <section id="homeMain">
@@ -43,7 +65,7 @@ function Home() {
         </div>
       </section>
       <section className="flex flex-col items-center h-fit">
-        <p className="text-darkBlue font-light leading-loose whitespace-pre-line text-center w-[90vw] py-4">
+        <p className={`text-darkBlue font-light leading-loose whitespace-pre-line text-center w-[90vw] py-4 `}>
           {`Hello and welcome! When I'm not pursuing one of my passions in the outdoors, you'll find me designing and brining user-centric web applications to life. I love to push my limits on a rock face, down a mountain bike trail and in software development. I believe that the same focus and determination required to conquer a challenging climb or trail translates seamlessly into the world of software development.`}
         </p>
         <div className="bg-teal h-fit w-screen flex flex-col-reverse md:flex-row items-center ">
@@ -53,7 +75,7 @@ function Home() {
             alt="Rick Vermeil head shot"
           />
           <div className="text-beige whitespace-pre-line ">
-            <p className="p-6 ">{`I'm a former marketing professional, with extensive experience in Project Management, Event Marketing, Web Content Management and Product Ownership, eager to transition to a Software Engineer role. While my background is in marketing, I recently attended the Turing School of Software and Design, successfully completing their frontend development program. This experience has given me the skills, knowledge and tenacity that I need to help me make this change.
+            <p ref={paragraphRef} className={`p-6 transition-all duration-700 ease-in-out transform ${isVisible ? 'translate-y-0 opacity-100' : 'translate-y-4 opacity-0'}`}>{`I'm a former marketing professional, with extensive experience in Project Management, Event Marketing, Web Content Management and Product Ownership, eager to transition to a Software Engineer role. While my background is in marketing, I recently attended the Turing School of Software and Design, successfully completing their frontend development program. This experience has given me the skills, knowledge and tenacity that I need to help me make this change.
 
             My expertise in modern Frontend languages and frameworks include proficiencies in React, JavaScript ES6, TypeScript, Sass, CSS3, HTML5, and testing with Cypress, Mocha/Chai and Jest but I am also always learning new tech. At the moment, I am learning Tailwind (which this website is styled with), Next.js and Node.js. UI/UX design, user-friendly error handling, responsive layouts, accessibility, Agile methodologies, and test-driven development are all important practices that I employ when creating applications.
 
