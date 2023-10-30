05669ae93cff30fb27dd0c644aeedac929b1e807
Rick Vermeil <rickv85@gmail.com>
2023-10-30 15:47:05 -0600

    Update resume and change download button text color

--- src/Components/Resume/Resume.tsx
+++ src/Components/Resume/Resume.tsx
@@ -20,12 +20,12 @@ export default function Resume({ setSection }: Props) {
         src="/assets/Rick Vermeil Resume.pdf"
         width="100%"
         height="100%"
-        title="Rick Vermeil Resume 10.19.23"
+        title="Rick Vermeil Resume 10.30.23"
       >
         Your browser does not support PDFs. Please download my resume by clicking the button below.
       </iframe>
       <button className=" h-fit w-fit bg-teal py-2 px-6 m-6 rounded-md ">
-        <a href={"/assets/Rick Vermeil Resume.pdf"} download style={{ textDecoration: 'none', color: 'inherit' }}>
+        <a href={"/assets/Rick Vermeil Resume.pdf"} download style={{ textDecoration: 'none', color: '#ffffff' }}>
           Download Resume
         </a>
       </button>
